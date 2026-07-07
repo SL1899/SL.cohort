1c8c1ab8e82a8866275c881776125804b93c6bd1
SL1899 <lannon1899@qq.com>
2026-07-07 17:05:30 +0800

    welcome

--- R/zzzzzz.R
+++ R/zzzzzz.R
@@ -4,8 +4,9 @@
 
 # --- 1. 定义包加载时的欢迎动作 ---
 .onAttach <- function(libname, pkgname) {
-  packageStartupMessage("==========================================\n")
+  packageStartupMessage("---------------------------------------\n")
   packageStartupMessage("  - SL.cohort (0.0.26.0707)")
-  packageStartupMessage("  - Wish you a nice day!")
-  packageStartupMessage("\n==========================================")
+  packageStartupMessage("  - designed by lannon1899@qq.com")
+  packageStartupMessage("  - My friend, wish you a nice day！ (●'◡'●)")
+  packageStartupMessage("\n---------------------------------------")
 }
