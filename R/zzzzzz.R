75822b8961c5463228c9635d424634d5eebe335f
SL1899 <lannon1899@qq.com>
2026-08-03 17:14:33 +0800

    version updated

--- R/zzzzzz.R
+++ R/zzzzzz.R
@@ -4,9 +4,12 @@
 
 # --- 1. 定义包加载时的欢迎动作 ---
 .onAttach <- function(libname, pkgname) {
+  # 获取当前包的动态版本号
+  pkg_version <- utils::packageVersion("SL.cohort")
+
   packageStartupMessage("---------------------------------------\n")
-  packageStartupMessage("  - SL.cohort (0.0.26.0707)")
-  packageStartupMessage("  - designed by lannon1899@qq.com")
-  packageStartupMessage("  - My friend, wish you a nice day！ (●'◡'●)")
+  packageStartupMessage("  - SL.cohort (", pkg_version, ")")
+  packageStartupMessage("  - Maintainer: lannon1899@qq.com")
+  packageStartupMessage("  - Have a wonderful day, my friend! (●'◡'●)")
   packageStartupMessage("\n---------------------------------------")
 }
